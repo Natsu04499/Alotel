1c214c3f0abcc3fdbcc64c833e245157b694e80c
natsu <natsu04499@gmail.com>
2023-03-30 14:21:40 +0200

    clean code

--- src/main/java/main/Home.java
+++ src/main/java/main/Home.java
@@ -88,7 +88,7 @@ public class Home extends JFrame {
                     }
                 }
             });
-//TESTlol
+
             menuBar.add(refreshButton);
 
             JScrollPane scrollPane = new JScrollPane(list); // Ajouter la liste à un JScrollPane
